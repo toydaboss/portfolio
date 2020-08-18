eb16e4de75b7859f17900640eec6ab1954ee0500
toydaboss <toyeseaderibibge@gmail.com>
2020-08-18 21:27:27 +0100

    scroll top animation fixed

--- main.js
+++ main.js
@@ -12,14 +12,24 @@ $(document).ready(function () {
             scrollTop: $($(this).attr('href')).offset().top - 100
         }, options: 1000});
     });
-    $('#up').on('click', function () {
-        $('html, body').animate({keyframes: {
-            scrollTop: 0
-        }, options: 1000});
-    });
-/*     AOS.init({
-        easing: 'ease',
-        duration: 500,
-        once: true
-    }); */
 });
+
+function scrollToTop (duration) {
+// cancel if already on top
+if (document.scrollingElement.scrollTop === 0) return;
+
+    const cosParameter = document.scrollingElement.scrollTop / 2;
+    let scrollCount = 0, oldTimestamp = null;
+
+    function step (newTimestamp) {
+        if (oldTimestamp !== null) {
+            // if duration is 0 scrollCount will be Infinity
+            scrollCount += Math.PI * (newTimestamp - oldTimestamp) / duration;
+            if (scrollCount >= Math.PI) return document.scrollingElement.scrollTop = 0;
+            document.scrollingElement.scrollTop = cosParameter + cosParameter * Math.cos(scrollCount);
+        }
+        oldTimestamp = newTimestamp;
+        window.requestAnimationFrame(step);
+    }
+        window.requestAnimationFrame(step);
+}
